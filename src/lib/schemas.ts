7ac8d1b87fee5f158429e3c32aca631b4fedb7cc
Marios Sofokleous <marios.sofokleous@yandex.com>
2025-06-17 22:37:49 +0300

    Add Zod schemas for data validation.

---
+++ src/lib/schemas.ts
@@ -0,0 +1,48 @@
+import { z } from "zod/v4";
+
+const linkSchema = z.object({
+  label: z.string(),
+  url: z.string(),
+  openLinkInNewTab: z.boolean(),
+  sameHostLink: z.boolean(),
+  showIcon: z.boolean(),
+  lucideIconComponentName: z.string(),
+});
+
+const imageSchema = z.object({
+  alternativeText: z.string(),
+  width: z.number(),
+  height: z.number(),
+  url: z.string(),
+});
+
+const socialItemSchema = z.object({
+  id: z.number(),
+  label: z.string(),
+  name: z.string(),
+  url: z.string(),
+});
+
+export const heroItemSchema = z.object({
+  id: z.number(),
+  preheading: z.string(),
+  heading: z.string(),
+  cta: linkSchema,
+  image: imageSchema
+});
+
+export const globalDataSchema = z.object({
+  navbar: z.object({
+    leftMenu: z.array(linkSchema.extend({ id: z.number() })),
+    rightMenu: z.array(linkSchema.extend({ id: z.number() })),
+    cta: linkSchema,
+    siteRepresentation: z.object({
+      logo: imageSchema,
+      socialChannels: z.array(socialItemSchema),
+    }),
+  }),
+});
+
+export const homeDataSchema = z.object({
+  hero: z.array(heroItemSchema),
+});
